6e81d7876f3e037d2405d0d183749b185a261432
aodun <aodun.czp@alipay.com>
2019-11-29 01:20:54 +0800

    change doman

--- webpack-dist.config.js
+++ webpack-dist.config.js
@@ -96,7 +96,7 @@ module.exports = {
     }),
     new EndWebpackPlugin(async () => {
       // 自定义域名
-      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'www.chenxiyao.com');
+      fs.writeFileSync(path.resolve(outputPath, 'CNAME'), 'www.chenzhipeng.vip');
 
       await publishGhPages();
 
